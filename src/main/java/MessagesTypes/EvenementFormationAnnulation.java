cf4039a0b5b2ba3c311f7d0731186360d90d285d
unknown <maxime.chaumes@gmail.com>
2019-11-14 16:18:42 +0100

    ajout Type message abstrait

---
+++ src/main/java/MessagesTypes/EvenementFormationAnnulation.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MessagesTypes;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Maxime
+ */
+public class EvenementFormationAnnulation extends EvenementFormation{
+    Date dateDebut;
+    int duree;
+    List<String> mails;
+
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public int getDuree() {
+        return duree;
+    }
+
+    public void setDuree(int duree) {
+        this.duree = duree;
+    }
+
+    public List<String> getMails() {
+        return mails;
+    }
+
+    public void setMails(List<String> mails) {
+        this.mails = mails;
+    }
+    
+    
+}
