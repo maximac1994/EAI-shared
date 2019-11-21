a4b0c0620522ebef093bda09de3ba930e3e31e4f
Mutator21 <jerome.lefebvre_28@hotmail.fr>
2019-11-21 13:39:34 +0100

    Ajout EvenementFormationProjet2

---
+++ src/main/java/MessagesTypes/EvenementFormationProjet2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MessagesTypes;
+
+import java.util.Date;
+
+/**
+ *
+ * @author jerom
+ */
+public class EvenementFormationProjet2 extends EvenementFormation {
+    
+    Date dateDebut;
+    int duree;
+
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    public int getDuree() {
+        return duree;
+    }
+
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public void setDuree(int duree) {
+        this.duree = duree;
+    }
+    
+    
+    
+}
