b3d93989c6cb3c3a63ba347cde5edeba5a00269c
unknown <maxime.chaumes@gmail.com>
2019-11-28 09:22:01 +0100

    evtProjet1

---
+++ src/main/java/MessagesTypes/EvenementFormationProjet1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MessagesTypes;
+
+/**
+ *
+ * @author Maxime
+ */
+public class EvenementFormationProjet1 extends EvenementFormation {
+    int idFormation;
+
+    public int getIdFormation() {
+        return idFormation;
+    }
+
+    public void setIdFormation(int idFormation) {
+        this.idFormation = idFormation;
+    }
+    
+}
